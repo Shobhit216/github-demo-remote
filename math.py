4f74b3097c99729b1d9f52c7d6e0a6201f99187a
Shobhit216 <65949677+Shobhit216@users.noreply.github.com>
2022-01-22 23:04:07 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+# Add implementation
+def add(x,y):
+    return x+y
+     
+# Subtract implementation
+def subtract(x,y):
+    return x-y       #on master branch
+    
+# Multiply implementation
+def multiply(x,y):
+    return x*y      #on Bug456 branch
+    
+# Divide implementation
+def divide(x,y):
+    if y==0:            #on master branch
+        return DIVIDE_BY_0_ERROR
+    else:
+        return x/y
